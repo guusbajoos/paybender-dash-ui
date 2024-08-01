4aad7eb122bae1d67d8e43433243f758e49680e1
fajriputra <fajriwn27@gmail.com>
2024-08-02 03:25:48 +0700

    initial project

---
+++ src/data/sidelinks.tsx
@@ -0,0 +1,29 @@
+import { IconHome } from '@tabler/icons-react'
+
+export interface NavLink {
+  title: string
+  label?: string
+  href: string
+  icon: JSX.Element
+}
+
+export interface SideLink extends NavLink {
+  sub?: NavLink[]
+}
+
+export const sidelinks: SideLink[] = [
+  {
+    title: 'Home',
+    label: '',
+    href: '',
+    icon: <IconHome size={18} color='#2A8F9B' />,
+    sub: [
+      {
+        title: 'Get started',
+        label: '',
+        href: '/',
+        icon: <></>,
+      },
+    ],
+  },
+]
