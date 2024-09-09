05f87efaaca3ff6e4d4b666c6bf24490090a38b6
AL FAJRI PUTRA PRATAMA <72371119+fajriputra@users.noreply.github.com>
2024-09-10 01:29:10 +0700

    remove router file

---
+++ src/router.tsx
@@ -0,0 +1,91 @@
+import { createBrowserRouter, Navigate, Outlet } from 'react-router-dom'
+import GeneralError from '@/pages/errors/general-error'
+import NotFoundError from '@/pages/errors/not-found-error'
+import MaintenanceError from '@/pages/errors/maintenance-error'
+import UnauthorisedError from '@/pages/errors/unauthorised-error.tsx'
+
+import AuthLayout from '@/components/partials/auth/auth-layout'
+import AuthGate from '@/components/partials/auth/auth-gate'
+
+const router = createBrowserRouter([
+  {
+    index: true,
+    element: <Navigate to='/auth/login' />,
+  },
+  {
+    path: 'auth',
+    element: (
+      <AuthLayout>
+        <Outlet />
+      </AuthLayout>
+    ),
+    children: [
+      {
+        path: 'login',
+        lazy: async () => ({
+          Component: (await import('@/pages/auth/login')).default,
+        }),
+      },
+      {
+        path: 'register',
+        lazy: async () => ({
+          Component: (await import('@/pages/auth/register')).default,
+        }),
+      },
+    ],
+  },
+  {
+    path: 'app',
+    element: (
+      <AuthGate>
+        <Outlet />
+      </AuthGate>
+    ),
+    // lazy: async () => {
+    //   const AppShell = await import('@/components/app-shell')
+    //   return { Component: AppShell.default }
+    // },
+    children: [
+      {
+        index: true,
+        path: 'get-started',
+        lazy: async () => ({
+          Component: (await import('@/pages/dashboard')).default,
+        }),
+        children: [
+          {
+            path: 'get-started/test-mode',
+            lazy: async () => ({
+              Component: (await import('@/pages/dashboard/test-mode')).default,
+            }),
+          },
+          {
+            path: 'get-started/test-mode/pay-in',
+            lazy: async () => ({
+              Component: (await import('@/pages/dashboard/test-mode/pay-in'))
+                .default,
+            }),
+          },
+          {
+            path: 'get-started/test-mode/pay-out',
+            lazy: async () => ({
+              Component: (await import('@/pages/dashboard/test-mode/pay-out'))
+                .default,
+            }),
+          },
+        ],
+      },
+    ],
+  },
+
+  // Error routes
+  { path: '/500', Component: GeneralError },
+  { path: '/404', Component: NotFoundError },
+  { path: '/503', Component: MaintenanceError },
+  { path: '/401', Component: UnauthorisedError },
+
+  // Fallback 404 route
+  { path: '*', Component: NotFoundError },
+])
+
+export default router
